43a1a044b908b5fa9beaa1216aef4e735306226e
geethu109 <103350499+geethu109@users.noreply.github.com>
2026-03-20 11:46:31 +0530

    odd recurring.py

--- odd recurring.py
+++ odd recurring.py
@@ -1,12 +1,12 @@
-arr=list(map(int, input().split())) # slow
+# code 1
+arr=list(map(int, input().split())) # slow but only single number will be displayed
 for i in range(len(arr)):
     if arr.count(arr[i])%2 !=0:
         print(arr[i])
-        
 
-
-# arr=list(map(int, input().split())) # fast
-# result=0
-# for i in arr:
-#     result^=i
-# print(result)
+# code 2        
+arr=list(map(int, input().split())) # fast but only single number will be displayed
+result=0
+for i in arr:
+    result^=i
+print(result)
